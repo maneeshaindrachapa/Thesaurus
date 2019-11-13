c7c7407bef7835e6ca01d42a511b65350cea23dd
ThilinaPrasad <thilinaprasad.15@cse.mrt.ac.lk>
2019-11-13 20:16:18 +0530

    Mobile app server config

--- MobileClient/src/app/services/serverConfig.ts
+++ MobileClient/src/app/services/serverConfig.ts
@@ -1,6 +1,6 @@
 export class ServerConfig {
-  // public host = 'http://thesaurus.projects.uom.lk';
-  public host = 'http://127.0.0.1';
+  public host = 'http://thesaurus.projects.uom.lk';
+  // public host = 'http://127.0.0.1';
   public port = '5000';
   // tslint:disable-next-line:variable-name
   public base_url = this.host + ':' + this.port;
